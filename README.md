e7ec4d532ed78a051eb3f887cecd42926436c46b
PCY <pcy4196@naver.com>
2022-03-04 23:30:03 +0900

    2022.03.04 commit 카카오 프렌즈 컬러링북(2) - programmers.level2.Q220304_001

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 ##### level2
 + 문자열 압축 - programmers.level2.Q220205_001
 + 오픈 채팅방 - programmers.level2.Q220214_001
-+ 카카오 프렌즈 컬러링북 - programmers.level2.Q220303_001
++ 카카오 프렌즈 컬러링북(1) - programmers.level2.Q220303_001
++ 카카오 프렌즈 컬러링북(2) - programmers.level2.Q220304_001
 ***
--- src/programmers/level2/Q220304_001.java
+++ src/programmers/level2/Q220304_001.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Q220303_001 {
+public class Q220304_001 {
 
-    // 프로그래머스 LEVEL2 카카오 프렌즈 컬러링북 - 1
+    // 프로그래머스 LEVEL2 카카오 프렌즈 컬러링북 - 2
     // 문제 URL : https://programmers.co.kr/learn/courses/30/lessons/1829
 
     // BFS(Breadth-First Search)[너비우선탐색]로 풀이하는 문제
@@ -16,6 +16,16 @@ public class Q220303_001 {
     int[] dy = {0,1, 0,-1};     // 상하좌우로 탐색할 수 있게 설정
     boolean[][] visited = null; // 해당 영역 방문 여부 체크
     int m, n;
+
+    class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public int[] solution(int m, int n, int[][] picture) {
         int numberOfArea = 0;               // 영역 변수(종료)
@@ -47,18 +57,18 @@ public class Q220303_001 {
         int target = picture[i][j];
         int areaSize = 1;
         this.visited[i][j] = true;
-        // Queue 선언 - [int 배열] :: class로도 선언 가능
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{i,j});    // queue에 값 설정
+        // Queue 선언 - [Point class로 선언]
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(i, j));    // queue에 값 설정
 
         while (!queue.isEmpty()) {
             // Queue에 값이 없을때까지 반복문 수행
-            int[] pointArr = queue.poll();
+            Point point = queue.poll();
 
             for (int k = 0; k < 4; k++) {
                 // i, j 기준점에서 상하좌우로 이동
-                int dx = pointArr[0] + this.dx[k];
-                int dy = pointArr[1] + this.dy[k];
+                int dx = point.x + this.dx[k];
+                int dy = point.y + this.dy[k];
 
                 // dx, dy 가 그림영역에 포함되고 값이 같으면
                 // areaSize 값을 중가
@@ -68,7 +78,7 @@ public class Q220303_001 {
                  && !this.visited[dx][dy]) {
                     this.visited[dx][dy] = true;
                     areaSize++;
-                    queue.offer(new int[]{dx,dy});
+                    queue.offer(new Point(dx, dy));
                 }
             }
         }
