be7e77723d681ede3f49d8ae3c507af8fe59b1ba
PCY <pcy4196@naver.com>
2022-04-11 22:31:54 +0900

    2022.04.11 commit 카펫 - programmers.level2.Q220410_001 -- 2

--- src/programmers/level2/Q220410_001.java
+++ src/programmers/level2/Q220410_001.java
@@ -15,8 +15,8 @@ public class Q220410_001 {
         // 가로 최소 길이 -- 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
         int minWidth = (int) Math.sqrt(area);
         // 약수 구하는 방식을 이용하여 가로길이 계산가능
-        // 가로의 최대값 - 넓이 / 2
-        for (int i = 3; i <= area / 2; i++) {
+        // 가로의 최대값 - 넓이 / 3 --> 제한조건에서 세로 최소값이 3이기 때문에
+        for (int i = 3; i <= area / 3; i++) {
             // i -> 약수이거나 루트값보다 크면 세로길이 계산 및 yellow 검증
             if (area % i == 0 && i >= minWidth) {
                 int height = area / i;
