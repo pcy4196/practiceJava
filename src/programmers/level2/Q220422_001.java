dc6d467252c16b5703aa581ed24aeb89f885bf94
PCY <pcy4196@naver.com>
2022-04-23 02:03:25 +0900

    2022.04.22 commit 다리를 지나는 트럭 - programmers.level2.Q220422_001 -- 2

--- src/programmers/level2/Q220422_001.java
+++ src/programmers/level2/Q220422_001.java
@@ -12,21 +12,56 @@ public class Q220422_001 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        // Queue 선언 및 데이터 적립
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < truck_weights.length; i++) {
-            queue.offer(truck_weights[i]);
-        }
-        while (!queue.isEmpty()) {
+        // 다리위에 있는 트럭
+        Queue<Integer> brideQ = new LinkedList<>();
 
+        int goneTruck = 0;      // 지나간 트럭 수
+        int sumWeight = 0;      // 다리에 있는 트럭 무게
+
+        while (true) {
+
+            if (brideQ.isEmpty()) {
+                answer++;
+                brideQ.offer(truck_weights[goneTruck]);
+                sumWeight += truck_weights[goneTruck];
+                goneTruck++;    // 대기중에서 다리로 지나간 트럭 수 증가
+            } else {
+
+                // 트럭이 다리로 다 지나갔으면 while문 종료
+                if (goneTruck == truck_weights.length)
+                    break;
+
+                // 다리에 있는 트럭이 다리를 지나갈거 같으면 총무게 트럭만큼 제외
+                if (brideQ.size() == bridge_length) {
+                    sumWeight -= brideQ.poll();
+                }
+
+                // 다음순서의 트럭을 합쳐도 다리무게의 허용범위 일 경우
+                if (sumWeight + truck_weights[goneTruck] <= weight) {
+                    answer++;
+                    sumWeight += truck_weights[goneTruck];
+                    brideQ.offer(truck_weights[goneTruck]);
+                    goneTruck++;        // 대기중에서 다리로 지나간 트럭 수 증가
+                // 허용범위를 넘겼을 경우에는 트럭이 지나갈때까지 대기
+                } else {
+                    answer++;
+                    brideQ.offer(0);
+                }
+
+            }
         }
-        return answer;
+
+        // 마지막 트럭이 다리에 올라간 상태에서 다리의 길이를 더해주면, 모든 트럭이 통과하는데 걸린 시간.
+        return answer + bridge_length;
     }
-
 
     @Test
     public void testSolution() {
-//        int[][] road = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
-//        Assertions.assertEquals(4, this.solution(5, road, 3));
+//        int[] truck_weights = {7,4,5,6};
+//        Assertions.assertEquals(8, this.solution(2, 10, truck_weights));
+//        int[] truck_weights = {10};
+//        Assertions.assertEquals(101, this.solution(100, 100, truck_weights));
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
+        Assertions.assertEquals(110, this.solution(100, 100, truck_weights));
     }
 }
