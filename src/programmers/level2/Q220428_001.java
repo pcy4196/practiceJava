cf0b153b146a281a6c321186a1372f2adcc3552b
PCY <pcy4196@naver.com>
2022-04-29 00:01:23 +0900

    2022.04.29 commit 다음 큰 숫자 - programmers.level2.Q220428_001 --> Integer.bitCount() 를 사용하여 효율성 문제 해결

--- src/programmers/level2/Q220428_001.java
+++ src/programmers/level2/Q220428_001.java
@@ -8,7 +8,8 @@ public class Q220428_001 {
     // 프로그래머스 LEVEL2 다음 큰 숫자
     // 문제 URL : https://programmers.co.kr/learn/courses/30/lessons/12911
 
-    public int solution(int n) {
+    // [1] - 효율성문제로 인해 다른 방식으로 해결 필요
+    public int solutionOne(int n) {
         int answer = 0;
         String biStr = Integer.toBinaryString(n);      // 10진수를 2진수로 변경하는 부분
         int cntOne = countStr(biStr, "1");        // 2진수에서 '1' 개수를 구하는 로직
@@ -28,9 +29,29 @@ public class Q220428_001 {
     private int countStr(String biStr, String str) {
         return biStr.length() - biStr.replace(str, "").length();
     }
+    // [1] - 효율성문제로 인해 다른 방식으로 해결 필요
+
+    public int solutionTwo(int n) {
+        int answer = 0;
+        int cntOne = Integer.bitCount(n);       // n 숫자를 binary로 변환 후 true(1) 값 반환
+
+        while (true) {
+            n++;    // 주어진 숫자를 증가
+            if (cntOne == Integer.bitCount(n)) {
+                // 증가한 숫자를 2진수로 변환 및 true(1)
+                // 갯수 반환하여 기준 숫자의 값과 비교
+                answer = n;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
 
     @Test
     public void testSolution() {
-        Assertions.assertEquals(83, this.solution(78));
+        Assertions.assertEquals(83, this.solutionOne(78));
+        Assertions.assertEquals(83, this.solutionTwo(78));
     }
 }
