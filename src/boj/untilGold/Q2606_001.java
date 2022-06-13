5854a7b00be20e2f70e9070f2835f6e456580257
PCY <pcy4196@naver.com>
2022-06-14 00:22:46 +0900

    2022.06.14 commit 바이러스(SILVER3) - boj.untilGold.Q2606_001 -- 2

--- src/boj/untilGold/Q2606_001.java
+++ src/boj/untilGold/Q2606_001.java
@@ -3,8 +3,7 @@ package boj.untilGold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q2606_001 {
 
@@ -15,10 +14,12 @@ public class Q2606_001 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());       // 컴퓨터의 수
+        st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());       // 컴퓨터 쌍의 수
         int[] visited = new int[N+1];                   // visited 배열 선언(접근기록)
+        Queue<Integer> queue = new LinkedList<>();
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {                   // 인접리스트 선언
+        for (int i = 0; i <= N; i++) {                   // 인접리스트 선언(초기화)
             graph.add(new ArrayList<>());
         }
 
@@ -30,6 +31,27 @@ public class Q2606_001 {
             graph.get(end).add(start);
         }
 
+        queue.add(1);               // 최초 시작 컴퓨터 1
+        visited[1] = 1;             // visited 배열변수에 값 대입
+        while (!queue.isEmpty()) {  // queue에 값이 없을 때 까지 while 문 진행
+            int com = queue.poll(); // queue에서 값 추출
+            for (Integer endCom : graph.get(com)) {
+                if (visited[endCom] != 1) {
+                    visited[endCom] = 1;
+                    queue.add(endCom);
+                }
+            }
+        }
+
+        int answer = 0;             // 감염 컴퓨터 수(1에서)
+        for (int i = 2; i <= N; i++) {
+            if (visited[i] == 1) {
+                answer++;
+            }
+        }
+
+        // 정답출력
+        System.out.println(answer);
     }
 
 }
