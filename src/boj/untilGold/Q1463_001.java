7bf46369210427753be6e14f6adc2c8a17083a7e
PCY <pcy4196@naver.com>
2022-06-18 00:42:37 +0900

    2022.06.17 commit 1로 만들기(SILVER3) - boj.untilGold.Q1463_001 -- 2

--- src/boj/untilGold/Q1463_001.java
+++ src/boj/untilGold/Q1463_001.java
@@ -14,13 +14,18 @@ public class Q1463_001 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());       // 계산을 해야하는 정수(1보다 크다)
+        int[] arr = new int[N+1];                       // 해당숫자를 구하기 위해 연산하는 수를 담는 배열변수
 
-        int answer = 0;
-        while (N > 1) {
-            answer++;
-            if (N % 3 == 0) {
-
+        for (int i = 2; i <= N; i++) {                  // bottom-up 방식으로 계산
+            arr[i] = arr[i-1] + 1;  // 해당 숫자가 2,3의 배수가 아닌 경우 이전 숫자의 값+1
+            if (i % 3 == 0) {       // 해당 숫자가 3의 배수이면 이전 숫자의 값+1, 이전 3의 배수 값+1
+                arr[i] = Math.min(arr[i/3] + 1, arr[i]);
+            }
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i/2] + 1, arr[i]);
             }
         }
+
+        System.out.println(arr[N]);
     }
 }
