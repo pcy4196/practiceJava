129edc482cca451355950e6fce57d7666df0e80e
PCY <pcy4196@naver.com>
2022-06-20 00:26:15 +0900

    2022.06.20 commit 계단 오르기(SILVER3) - boj.untilGold.Q2579_001 -- 2

--- src/boj/untilGold/Q2579_001.java
+++ src/boj/untilGold/Q2579_001.java
@@ -14,15 +14,21 @@ public class Q2579_001 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());       // 계단의 개수
-        int[] arr = new int[N];                         // 계단의 점수를 보관하는 배열
-        for (int i = 0; i < N; i++) {
+        int[] arr = new int[N+1];                       // 계단의 점수를 보관하는 배열
+        int[] dy  = new int[N+1];                       // 해당 계단까지의 최대 점수(Memoization)
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-//        for (int i : arr) {
-//            System.out.println(i);
-//        }
+        dy[1] = arr[1];             // 첫번째 계단
+        dy[2] = arr[1] + arr[2];    // 두번째 계단
+        for (int i = 3; i <= N; i++) {
+            // 두칸전(i - 2)의 'Memoization'값
+            // 한칸전(i - 1)의 값 + 세칸전(i - 3)의 'Memoization' 값 중 큰 값
+            dy[i] = arr[i] + Math.max(dy[i-2], dy[i-3] + arr[i-1]);
+        }
 
+        System.out.println(dy[N]);
     }
 }
