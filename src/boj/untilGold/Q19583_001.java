7f3f78834b247e26575190cb0d4512e10fa7c11f
PCY <pcy4196@naver.com>
2022-07-05 01:03:06 +0900

    2022.07.05 commit 싸이버개강총회(SILVER2) - boj.untilGold.Q19583_001 -- 3

--- src/boj/untilGold/Q19583_001.java
+++ src/boj/untilGold/Q19583_001.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Q19583_001 {
@@ -12,20 +14,36 @@ public class Q19583_001 {
     // 문제 URL : https://www.acmicpc.net/problem/19583
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<String, String> map = new HashMap<>();
-        String S = st.nextToken().replace(":", "");       // 개강총회를 시작한 시간 S
-        String E = st.nextToken().replace(":", "");       // 개강총회를 끝낸 시간 E
-        String Q = st.nextToken().replace(":", "");       // 개강총회 스트리밍을 끝낸 시간 Q
+        Scanner sc = new Scanner(System.in);
+        HashSet<String> in = new HashSet<>();       // 입장
+        HashSet<String> out = new HashSet<>();      // 퇴장
+        HashSet<String> name = new HashSet<>();     // 회원아이디
 
-//        String line = null;
-//        while ((line = br.readLine()) != null) {
-//            st = new StringTokenizer(line);
-//
-//        }
-        System.out.println(S);
-        System.out.println(E);
-        System.out.println(Q);
+        String[] fLine = sc.nextLine().split(" ");
+        String S = fLine[0].replace(":", "");   // 개강총회 시작
+        String E = fLine[0].replace(":", "");   // 개강총회 종료
+        String Q = fLine[0].replace(":", "");   // 스트리밍 종료
+        while(sc.hasNextLine()) {
+            String[] input = sc.nextLine().split(" ");
+            if (input.length > 1) {
+                name.add(input[1]);
+                String time = input[0].replace(":", "");
+                if (time.compareTo(S) <= 0) {
+                    in.add(input[1]);
+                } else if (time.compareTo(E) >= 0
+                        && time.compareTo(Q) >= 0) {
+                    out.add(input[1]);
+                }
+            }
+        }
+
+        int answer = 0;
+        for (String s : name) {
+            if (in.contains(s) && out.contains(s)) {
+                answer++;   // 회원아이디가 입장, 퇴장 둘다 있을 경우
+            }
+        }
+
+        System.out.println(answer);
     }
 }
