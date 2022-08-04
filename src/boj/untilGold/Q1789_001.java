46c47b00ebc76330e0225f41557defc56d225705
PCY <pcy4196@naver.com>
2022-08-05 00:10:44 +0900

    2022.08.05 commit 수들의 합(SILVER5) - boj.untilGold.Q1789_001 -- 2

--- src/boj/untilGold/Q1789_001.java
+++ src/boj/untilGold/Q1789_001.java
@@ -13,9 +13,19 @@ public class Q1789_001 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
+        long S = Long.parseLong(st.nextToken());
 
-        System.out.println(S);
+        // 1~N 까지의 합이 S 보다 클 경우까지 더해주는 로직 구현
+        long sum = 0;
+        long N = 0;
+        while (sum < S) {
+            N++;
+            sum += N;
+        }
+
+        // 1~N 까지의 합이 S랑 다른 경우 더했던 수 중에서
+        // 한 수를 마이너스하기 때문에 N-1개룰 출력
+        System.out.println(sum == S ? N : N - 1);
     }
 
 }
