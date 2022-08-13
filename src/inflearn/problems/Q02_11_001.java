f8a179f9c5be0be1decd4a078d0139d0e623f075
PCY <pcy4196@naver.com>
2022-08-13 14:58:06 +0900

    2022.08.13 commit 02-11(임시반장 정하기) - inflearn.problems.Q02_11_001 -- 2

--- src/inflearn/problems/Q02_11_001.java
+++ src/inflearn/problems/Q02_11_001.java
@@ -3,7 +3,6 @@ package inflearn.problems;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q02_11_001 {
@@ -14,8 +13,8 @@ public class Q02_11_001 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n+1][6];      // 1번학생, 1학년~
+        int n = Integer.parseInt(st.nextToken());       // 총 학생수
+        int[][] arr = new int[n+1][6];                  // 1번학생, 1학년~
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= 5; j++) {
@@ -23,10 +22,30 @@ public class Q02_11_001 {
             }
         }
 
-//        for (int i = 1; i <= n; i++) {
-//            System.out.println(Arrays.toString(arr[i]));
-//        }
+        int samCnt = 0;             // 같은 반 했던 수
+        int max = 0;                // 가장 큰 수(같은 반 했던 수)
+        int answer = 1;             // 같은 사람이 없을 경우 1을 반환
+        for (int i = 1; i <= n; i++) {
+            samCnt = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        samCnt++;
+                        break;
+                    }
+                }
+            }
+            // (i)학생의 임시반장 선정여부 확인 로직
+            if (samCnt > max) {
+                max = samCnt;
+                answer = i;
+            }
+        }
 
+        System.out.println(answer);
     }
 
 }
