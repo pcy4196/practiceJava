6bf08c2896c69661dd6a806fec5c3a9efdf911ae
PCY <pcy4196@naver.com>
2022-08-27 00:52:26 +0900

    2022.08.27 commit 05-08(응급실) - inflearn.problems.Q05_08_001 - 003

--- src/inflearn/problems/Q05_08_001.java
+++ src/inflearn/problems/Q05_08_001.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q05_08_001 {
@@ -49,8 +47,8 @@ public class Q05_08_001 {
 
     // 환자의 정보를 가지는 클래스
     static class Elem {
-        private int num;
-        private int idx;
+        int num;
+        int idx;
 
         public Elem(int num, int idx) {
             this.num = num;
