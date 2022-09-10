93c942e615fd3fceec16ff30b9931339f9eca567
PCY <pcy4196@naver.com>
2022-09-11 00:13:00 +0900

    2022.09.11 commit 인구 이동(GOLD5) - boj.untilGold.Q16234_001 -- 002

--- src/boj/untilGold/Q16234_001.java
+++ src/boj/untilGold/Q16234_001.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -16,8 +17,11 @@ public class Q16234_001 {
     static int R;
     static int[][] con;         // 인구수를 담는 배열변수
     static int[][] visited;     // 나라 방문여부
+    static boolean combYn = false;  // 연합여부
     static Queue<Con> queue;    // BFS 진행
     static ArrayList<Con> list; // 연합할 나라를 담는 List
+    static int[] dx = { 0, 1, 0, -1 }; // BFS 이동 메서드
+    static int[] dy = { 1, 0, -1, 0 }; // 동 남 서 북
 
     // 각나라의 위치를 담는 class 변수 선언
     static class Con {
@@ -49,27 +53,69 @@ public class Q16234_001 {
         // 1. 연합국가 탐색(BFS)
         // 1-1. 탐색 시 연합국가별 인구합계 및 인구이동할 국가 체크(ArraysList 사용)
         // 2. 인구 이동
-        // 3. 1~2 반복 --> 반복시 인구이도할 국가가 없으면 반복 종료
+        // 3. 1~2 반복 --> 반복시 인구이동할 국가가 없으면 반복 종료
+
+        // 정답 출력 변수
+        int result = 0;
 
         do {
             // 방문여부 변수 초기화
             visited = new int[N][N];
-            int sum = BFS(0, 0);
-            if (list.size() > 0) {
-                // 2. 인구 이동
-                // movePeopl(sum);
+            combYn = false;
+            // 연합발생여부 확인(하루)
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (visited[i][j] == 0) {
+                        // 1. 연합국가 탐색(BFS)
+                        int sum = BFS(i, j);
+                        // 2. 인구 이동
+                        if (list.size() > 1) {
+                            movePeopl(sum);
+                            combYn = true;
+                        }
+                    }
+                }
             }
-        } while (list.size() > 0);
+            if (combYn) {
+                result++;
+            }
+        } while (combYn);
 
+        System.out.println(result);
+    }
+
+    // 인구이동 발생
+    private static void movePeopl(int sum) {
+        int newNum = sum / list.size();
+        for (int i = 0; i < list.size(); i++) {
+            con[list.get(i).x][list.get(i).y] = newNum;
+        }
     }
 
     private static int BFS(int i, int j) {
         // 1-1. 탐색 시 연합국가별 인구합계 및 인구이동할 국가 체크(ArraysList 사용)
-        int sum = 0;
+        list = new ArrayList<>();
+        list.add(new Con(i, j));
+        queue = new LinkedList<>();
         queue.add(new Con(i, j));
-        while (!queue.isEmpty()) {
-            Con con = queue.poll();
+        visited[i][j] = 1;
+        int sum = con[i][j];
 
+        while (!queue.isEmpty()) {
+            Con c = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = c.x + dx[k];
+                int ny = c.y + dy[k];
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    int diff = Math.abs(con[c.x][c.y] - con[nx][ny]);
+                    if (diff >= L && diff <= R && visited[nx][ny] == 0) {
+                        visited[nx][ny] = 1;        // 해당 나라 방문여부 변경
+                        queue.add(new Con(nx, ny));
+                        list.add(new Con(nx, ny));
+                        sum += con[nx][ny];
+                    }
+                }
+            }
         }
 
 
